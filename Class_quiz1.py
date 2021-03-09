b73d826b85efa1684422661c0e0e118060146d85
An <dksdndyd123@gmail.com>
2021-03-09 11:36:38 +0900

    test1

---
+++ Class_quiz1.py
@@ -0,0 +1,18 @@
+'''
+1.
+계산기 기능을 하는 Calculator 클래스를 만드시오.
+
+cal = Calculator()
+<입력>
+print(cal.Add(10,20))
+print(cal.Min(10,20))
+print(cal.Mul(10,20))
+print(cal.Div(10,20))
+
+<출력>
+30
+-10
+200
+0.5
+
+'''
